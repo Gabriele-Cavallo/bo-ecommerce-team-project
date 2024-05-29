dee4ab5c5a72e9878368679b3b7cecbe18f7d2d2
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-05-29 17:31:35 +0200

    fix: customers table

--- database/migrations/2024_05_29_153021_create_customers_table.php
+++ database/migrations/2024_05_29_153021_create_customers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
             $table->string('lastname', 50);
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('customers');
     }
 };
